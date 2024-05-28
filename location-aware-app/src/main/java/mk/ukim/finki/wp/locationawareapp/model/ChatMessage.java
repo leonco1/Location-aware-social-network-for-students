df4f7169e51bdde97e9f044ce693a3416b034ba6
Leonco1 <leonid20021@gmail.com>
2024-05-28 23:11:54 +0200

    Added functional chat-room

--- location-aware-app/src/main/java/mk/ukim/finki/wp/locationawareapp/model/ChatMessage.java
+++ location-aware-app/src/main/java/mk/ukim/finki/wp/locationawareapp/model/ChatMessage.java
@@ -4,20 +4,13 @@ import jakarta.persistence.*;
 import lombok.*;
 import mk.ukim.finki.wp.locationawareapp.model.Enum.MessageType;
 
-@Data
-@Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class ChatMessage {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
     private String content;
     private String sender;
-    @Enumerated(EnumType.STRING)
     private MessageType messageType;
 }

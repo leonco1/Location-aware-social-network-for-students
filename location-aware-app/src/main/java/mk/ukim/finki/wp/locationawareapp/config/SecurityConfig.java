4ffa5e9331e9d237f193ec5ffaf36f9e80fb1ede
Leonco1 <leonid20021@gmail.com>
2024-05-28 20:34:41 +0200

    Added IPFilter to filter anyone not on the same Network

--- location-aware-app/src/main/java/mk/ukim/finki/wp/locationawareapp/config/SecurityConfig.java
+++ location-aware-app/src/main/java/mk/ukim/finki/wp/locationawareapp/config/SecurityConfig.java
@@ -1,5 +1,13 @@
 package mk.ukim.finki.wp.locationawareapp.config;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import mk.ukim.finki.wp.locationawareapp.model.exceptions.NoWifiFoundException;
+import mk.ukim.finki.wp.locationawareapp.service.WifiService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,17 +20,26 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.filter.GenericFilterBean;
+
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig{
     private  final PasswordEncoder passwordEncoder;
     private final UserDetailsService userDetailsService;
+    private final WifiService wifiService;
+    private final String ip_address;
 
-    public SecurityConfig(UserDetailsService userDetailsService,PasswordEncoder passwordEncoder) {
+
+    public SecurityConfig(UserDetailsService userDetailsService,PasswordEncoder passwordEncoder,WifiService wifiService) throws IOException {
         this.userDetailsService = userDetailsService;
        this.passwordEncoder=passwordEncoder;
+       this.wifiService=wifiService;
+       this.ip_address=wifiService.getIpAddress().orElseThrow(NoWifiFoundException::new);
     }
 
 
@@ -32,6 +49,7 @@ public class SecurityConfig{
 
         http
                 .csrf(AbstractHttpConfigurer::disable)
+               .addFilterBefore(new IPFilter(), UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests( (requests) -> requests
                         .requestMatchers(AntPathRequestMatcher.antMatcher("/"))
                         .permitAll()
@@ -54,7 +72,22 @@ public class SecurityConfig{
         return http.build();
     }
 
-    @Bean
+    private class IPFilter extends GenericFilterBean {
+        @Override
+        public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException, ServletException {
+            HttpServletRequest httpRequest = (HttpServletRequest) request;
+            String clientIp = httpRequest.getRemoteAddr();
+            boolean allowed = clientIp.startsWith(ip_address)||clientIp.equals("0:0:0:0:0:0:0:1");
+            if (allowed) {
+                chain.doFilter(request, response);
+            } else {
+                HttpServletResponse httpResponse = (HttpServletResponse) response;
+                httpResponse.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                httpResponse.getWriter().write("Access Forbidden");
+            }
+        }
+    }
+        @Bean
     public AuthenticationManager authManager(HttpSecurity http) throws Exception {
         AuthenticationManagerBuilder authenticationManagerBuilder =
                 http.getSharedObject(AuthenticationManagerBuilder.class);
--- location-aware-app/src/main/java/mk/ukim/finki/wp/locationawareapp/service/impl/WifiServiceImpl.java
+++ location-aware-app/src/main/java/mk/ukim/finki/wp/locationawareapp/service/impl/WifiServiceImpl.java
@@ -4,6 +4,7 @@ import mk.ukim.finki.wp.locationawareapp.model.exceptions.NoWifiFoundException;
 import mk.ukim.finki.wp.locationawareapp.service.WifiService;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -15,16 +16,30 @@ public class WifiServiceImpl implements WifiService {
         Process process = Runtime.getRuntime().
                 exec("ipconfig");
         BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        return reader.lines().filter(i->i.trim().startsWith("IPv4 Address")).map(i->i.split(":")[1].trim()).findFirst();
+        return TrimIpAddress(reader.lines().filter(i->i.trim().startsWith("Wireless LAN adapter Wi-Fi:"))
+                .flatMap(section -> reader.lines().skip(1).takeWhile(line -> !line.trim().isEmpty()))
+                .filter(i->i.trim().startsWith("IPv4 Address"))
+                .map(i->i.split(":")[1].trim()).findFirst());
     }
-
+    private Optional<String> TrimIpAddress(Optional<String> ipaddress)
+    {
+        String ip_address=ipaddress.orElseThrow(NoWifiFoundException::new);
+        System.out.println(ip_address);
+        String[]ip_parts=ip_address.split("\\.");
+        StringBuilder new_ip=new StringBuilder();
+        for(int i=0;i<3;i++)
+        {
+            new_ip.append(ip_parts[i]);
+            new_ip.append(".");
+        }
+        new_ip.deleteCharAt(new_ip.length()-1);
+        return Optional.of(new_ip.toString());
+    }
     @Override
     public void SendMessage() throws IOException {
         String ip_address=this.getIpAddress().orElseThrow(NoWifiFoundException::new);
         String subnet_mask=this.getSubnetMask().orElseThrow(NoWifiFoundException::new);
         int subnet_length=CalculateSubnetLength(subnet_mask);
-
-
     }
 
     private Integer CalculateSubnetLength(String subnet)

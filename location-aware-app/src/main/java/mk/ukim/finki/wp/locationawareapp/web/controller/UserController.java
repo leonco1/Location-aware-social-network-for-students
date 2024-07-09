860a0523562fef639ebc7c9bd372facaf5ac792c
Leonco1 <leonid20021@gmail.com>
2024-07-09 13:04:42 +0200

    Added exit button and different functionalities for admin and user exit

--- location-aware-app/src/main/java/mk/ukim/finki/wp/locationawareapp/web/controller/UserController.java
+++ location-aware-app/src/main/java/mk/ukim/finki/wp/locationawareapp/web/controller/UserController.java
@@ -1,17 +1,31 @@
 package mk.ukim.finki.wp.locationawareapp.web.controller;
 
 import jakarta.servlet.http.HttpSession;
+import mk.ukim.finki.wp.locationawareapp.model.ChatMessage;
+import mk.ukim.finki.wp.locationawareapp.model.Enum.MessageType;
+import mk.ukim.finki.wp.locationawareapp.model.Enum.Role;
 import mk.ukim.finki.wp.locationawareapp.model.User;
 import mk.ukim.finki.wp.locationawareapp.service.UserService;
 import mk.ukim.finki.wp.locationawareapp.service.WifiService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import static mk.ukim.finki.wp.locationawareapp.LocationAwareAppApplication.exitApp;
 
 @Controller
 @RequestMapping("/")
 public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping
     public String getHomePage(HttpSession session, Model model)
     {
@@ -23,4 +37,16 @@ public class UserController {
             return "access-denied"; // Return a view indicating access is denied
         }
     }
-}
+    @PostMapping("/exit")
+    public String exit(@RequestParam String role, @RequestParam String username) {
+        if (role.equals(Role.ROLE_USER.getAuthority())) {
+            userService.removeUser(username);
+            return "index-chat-page";
+        } else if (role.equals(Role.ROLE_ADMIN.getAuthority())) {
+            userService.removeUser(username);
+            exitApp();
+        }
+        return "index-chat-page";
+    }
+    }
+
